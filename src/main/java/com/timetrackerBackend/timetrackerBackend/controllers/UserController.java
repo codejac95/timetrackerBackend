9461c23220e9463d352f39c406158da44bec1e8c
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2024-05-07 13:25:43 +0200

    fix

--- src/main/java/com/timetrackerBackend/timetrackerBackend/controllers/UserController.java
+++ src/main/java/com/timetrackerBackend/timetrackerBackend/controllers/UserController.java
@@ -1,9 +1,6 @@
 package com.timetrackerBackend.timetrackerBackend.controllers;
 
 import java.util.List;
-import java.util.Map;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
